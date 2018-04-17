4f3ad9d3ed04c9eb393e0dd34ab0cd2b2e3d87c9
benjaminDELPECH <benjamin.delpech66@gmail.com>
2018-04-17 11:29:30 +0200

    creation des types pour semaine, jour et creneaux
    creation des types pour semaine, jour et creneaux

--- sem_j_cren.ads
+++ sem_j_cren.ads
@@ -3,5 +3,35 @@ use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
 
 package sem_j_cren is 
    
+    T_Creneau_Aqua is record
+      Tabinscrit:T_Tabadherents(1..N);
+      Taille:Integer;=0;
+      Open:Boolean;
+   end record;
+
+
+   T_Creneau_Fitness is record
+      Tabinscrit:T_Tabadherents(1..N);
+      Taille:Integer;=0;
+      Open:Boolean;
+   end record;
+      
+   
+   T_Creneau is record
+      Aqua:T_Creneau_Aqua;
+      Fitness:T_Creneau_Fitness;
+   end record;
+         
+      
+   
+      
+      
+end record;
+      
+Type T_Jour is array(Integer range 1..6)of T_Creneau;
+Type T_Semaine is array(lundi,mardi,mercredi,jundi,vendredi,samedi,dimanche);
+
+Type T_Planning is array(Integer range 1..6)of T_Jour;
+
       
 end sem_j_cren;
