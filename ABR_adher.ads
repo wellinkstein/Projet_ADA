22a2fe1220d00d536a0b8b87527832074b944557
benjaminDELPECH <benjamin.delpech66@gmail.com>
2018-04-17 11:29:47 +0200

    arbre d'adherents
    arbres d'adherents

--- ABR_adher.ads
+++ ABR_adher.ads
@@ -1,7 +1,14 @@
 with Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
 use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
 
-package sem_j_cren is 
+package ABR_adher is 
+   type T_Noeud;
+   type T_Arbre_adh is Acess T_Noeud;
+   type T_Noeud is record
+      Adh:T_Adherent;
+      Fg,Fd:T_Arbre_adh;
+   end record;
+   
    
       
-end sem_j_cren;
+end ABR_adher;
