f09770d24125f31412881b03f0c8b6683a3b7abe
benjaminDELPECH <benjamin.delpech66@gmail.com>
2018-04-19 00:07:05 +0200

    modif
    modif

--- ABR_adher.ads
+++ ABR_adher.ads
@@ -3,7 +3,7 @@ use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
 
 package ABR_adher is 
    type T_Noeud;
-   type T_Arbre_adh is Acess T_Noeud;
+   type T_Arbre_adh is acess T_Noeud;
    type T_Noeud is record
       Adh:T_Adherent;
       Fg,Fd:T_Arbre_adh;
