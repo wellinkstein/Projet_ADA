1b97b40b0c6f287b35b0c2c67480e8b3ad072d48
benjaminDELPECH <benjamin.delpech66@gmail.com>
2018-04-19 01:21:19 +0200

    modif
    modif

--- fichiers.ads
+++ fichiers.ads
@@ -1,7 +1,21 @@
-with Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
-use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
+with Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io,Ada.Sequential_IO,declaration_adherent,gestion_pile;
+use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io,declaration_adherent,gestion_pile;
 
-package fichiers is 
+package Fichiers is 
+   
+
+ 
+
+   type T_Tableau_Adherents is array(Integer range 1..N)of T_Adherent;
+   Tab_Adh:T_Tableau_Adherents;
+   tete_pile:T_Pteurpileadherents;
+
+PACKAGE Fichier_Adherents IS NEW Ada.Sequential_IO(T_tableau_Adherents);
+use Fichier_Adherents;
+PROCEDURE Save_Adherents(Adherents: IN T_Tableau_Adherents);
+PROCEDURE Restore_Adherents(Adherents:OUT T_Tableau_Adherents);
    
       
+
+
 end fichiers;
