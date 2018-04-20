5ec13b8c544f471aa3e89088f5f6cc062823fabc
defragolo <35921791+defragolo@users.noreply.github.com>
2018-04-20 17:11:49 +0100

    Update ABR_adher.ads

--- ABR_adher.ads
+++ ABR_adher.ads
@@ -1,9 +1,8 @@
-with Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
-use Ada.Text_Io,Ada.Integer_Text_Io,Ada.Float_Text_Io;
+with gestion_pile;
 
 package ABR_adher is 
    type T_Noeud;
-   type T_Arbre_adh is acess T_Noeud;
+   type T_Arbre_adh is access T_Noeud;
    type T_Noeud is record
       Adh:T_Adherent;
       Fg,Fd:T_Arbre_adh;
